bd128ae45ec94e7117ba3e94cf69d5d17bbca7aa
Goffigus <56737195+Goffigus@users.noreply.github.com>
2019-10-30 14:54:28 -0400

    Add project files.

---
+++ FunFactorial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+namespace FunFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userNum;
+            int fact = 1;
+
+            while (fact > 0)
+            {
+                userNum = ReadLine();
+                fact = Convert.ToInt32(userNum);
+            
+                for (int n = fact - 1; n > 1; n--)
+                {
+                    fact = fact * n; // fact *= n
+
+                }
+
+                WriteLine(fact);
+            }
+        }
+    }
+}
